5030a1d182e38bb5b974f0e1d67439678d929551
Lovethrech <rachealdolapo45@gmail.com>
2025-07-28 11:54:18 +0100

    ...working on authentication page.

--- src/components/auth/AuthLogin.vue
+++ src/components/auth/AuthLogin.vue
@@ -2,7 +2,9 @@
     <div class="auth-login-main-ctn">
         <form action="" class="auth-login-main-ctn-form">
             <div class="auth-login-main-ctn-form-input-and-label-ctn">
-
+                <label for="" class="input-ctn-name"></label>
+                <input v-model="email" id="" type="" :style="{border: inputCtnBorderAlertStyle}" required/>
+                
             </div>
             <div class="auth-login-main-ctn-form-input-and-label-ctn">
                 
