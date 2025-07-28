8e087c7cad39acb7adc39cd7ba8c074a28927c3a
Lovethrech <rachealdolapo45@gmail.com>
2025-07-28 10:50:15 +0100

    ...starting the project.

--- src/components/LoadPage.vue
+++ src/components/LoadPage.vue
@@ -8,6 +8,7 @@
 .load-page-main-ctn{
     width:100%;
     height: 100vh;
-    background: red url(/LoadPageTopLeaf.png);
+    background:  url(/LoadPageTopLeaf.png) top right / 100px no-repeat, 
+        url(/LoadPageBottomLeaf.png) bottom left / 100px no-repeat ;
 }
 </style>
