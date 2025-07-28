1d26b4d6343110a5a3b34aa53066e2ec38c532e4
Lovethrech <rachealdolapo45@gmail.com>
2025-07-28 11:39:45 +0100

    ...working on authentication page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import LoadPage from '@/components/LoadPage.vue';
+
+const showComponent = ref(true)
+let timeoutId = null
+onMounted(() => {
+  timeoutId = setTimeout(() => {
+    showComponent.value = false
+  }, 10000) // 5 seconds
+})
+onUnmounted(() => {
+  clearTimeout(timeoutId)
+});
 </script>
 
 <template>
