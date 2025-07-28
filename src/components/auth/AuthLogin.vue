42dca203c964a7e3a76528fc38fb3b649ce145e8
Lovethrech <rachealdolapo45@gmail.com>
2025-07-28 11:52:05 +0100

    ...working on authentication page.

--- src/components/auth/AuthLogin.vue
+++ src/components/auth/AuthLogin.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="auth-login-main-ctn">
         <form action="" class="auth-login-main-ctn-form">
-            
+            <div class="auth-login-main-ctn-form-input-and-label-ctn">
+
+            </div>
+            <div class="auth-login-main-ctn-form-input-and-label-ctn">
+                
+            </div>
         </form>
     </div>
 </template>
