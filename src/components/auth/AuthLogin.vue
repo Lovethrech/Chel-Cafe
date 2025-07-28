d4e4f9cb83c9834c49dd13f9de24f700f25e4013
Lovethrech <rachealdolapo45@gmail.com>
2025-07-28 11:55:59 +0100

    ...working on authentication page.

--- src/components/auth/AuthLogin.vue
+++ src/components/auth/AuthLogin.vue
@@ -1,8 +1,14 @@
+<script setup>
+import {ref} from "vue";
+
+const inputCtnBorderAlertStyle=ref('3px solid #ededed');
+</script>
+
 <template>
     <div class="auth-login-main-ctn">
         <form action="" class="auth-login-main-ctn-form">
             <div class="auth-login-main-ctn-form-input-and-label-ctn">
-                <label for="" class="input-ctn-name"></label>
+                <label for="" class="input-ctn-name">Username</label>
                 <input v-model="email" id="" type="" :style="{border: inputCtnBorderAlertStyle}" required/>
                 
             </div>
