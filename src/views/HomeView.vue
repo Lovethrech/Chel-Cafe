11b4f2e30a34182c4a84f777d26032ff074bda48
Lovethrech <rachealdolapo45@gmail.com>
2025-07-28 13:20:16 +0100

    ...working on authentication page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ let timeoutId = null
 onMounted(() => {
   timeoutId = setTimeout(() => {
     showComponent.value = false
-  }, 2000) // 2 seconds
+  }, 3000) // 3 seconds
 })
 onUnmounted(() => {
   clearTimeout(timeoutId)
