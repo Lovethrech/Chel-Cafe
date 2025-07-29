1103a63cd3fe052c823426be62aae33a7b1915f2
Lovethrech <rachealdolapo45@gmail.com>
2025-07-29 20:34:35 +0100

    ...working on authentication page.

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: '/home',
       name: 'home',
-      component: () => import('../views/AuthPageView.vue'),
+      component: () => import('../views/View.vue'),
     }
   ]
 })
