9fa13e3cda91e157e4b9a67e2d076cb23d9154c6
Lovethrech <rachealdolapo45@gmail.com>
2025-07-30 20:28:43 +0100

    ...working on authentication page.

--- src/components/auth/AuthSignUpType.vue
+++ src/components/auth/AuthSignUpType.vue
@@ -1,7 +1,7 @@
 <template>
     <a href="" class="auth-sign-up-type-main-ctn-link">
         <div class="auth-sign-up-type-main-ctn">
-            
+            <img src="@/components/Icons/apple.svg" alt="">
         </div>
     </a>
 </template>
