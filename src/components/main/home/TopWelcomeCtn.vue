6cc242cb507a6f0d6776d67f640db6946c10aeb3
Lovethrech <rachealdolapo45@gmail.com>
2025-08-20 15:38:37 +0100

    ...working on the project.

--- src/components/main/home/TopWelcomeCtn.vue
+++ src/components/main/home/TopWelcomeCtn.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="main-top-welcome-ctn-main-ctn">
-        .main-top-welcome-ctn-mini-ctn
+        <div class="main-top-welcome-ctn-mini-ctn"></div>
     </div>
 </template>
