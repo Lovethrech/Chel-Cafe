846ca55b6db8194f767eedaa9c32be8f546651cc
Lovethrech <rachealdolapo45@gmail.com>
2025-08-21 15:43:48 +0100

    ...working on the project.

--- src/components/main/home/MainCtn.vue
+++ src/components/main/home/MainCtn.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import TopWelcomeCtn from './TopWelcomeCtn.vue';
 </script>
 
 <template>
     <div class="main-home-main-ctn">
-
+        <TopWelcomeCtn></TopWelcomeCtn>
     </div>
 </template>
