f1a4d0e0ce15151feabb0cd50e600e1cb35b0bdc
Lovethrech <rachealdolapo45@gmail.com>
2025-08-21 16:41:03 +0100

    ...working on the project.

--- src/components/main/home/TopWelcomeCtn.vue
+++ src/components/main/home/TopWelcomeCtn.vue
@@ -31,4 +31,14 @@ import OrderTab from './nav/OrderTab.vue';
     text-align: left;
     font-size: 36px;
 }
+
+@media screen and (max-width: 440px) {
+    .main-top-welcome-ctn-main-ctn{
+        background:  url(/LoadPageTopLeaf.png) #F7E7CE top right / 50px no-repeat;
+
+    }
+    .main-top-welcome-ctn-mini-ctn-title{
+        font-size: 26px;
+    }
+}
 </style>
