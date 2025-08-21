11377aab8d438818834d20e22a467de56df11493
Lovethrech <rachealdolapo45@gmail.com>
2025-08-21 15:50:39 +0100

    ...working on the project.

--- src/components/main/home/TopWelcomeCtn.vue
+++ src/components/main/home/TopWelcomeCtn.vue
@@ -10,6 +10,7 @@
 .main-top-welcome-ctn-main-ctn{
     width:100%;
     height:20vh;
-    background:  url(/LoadPageTopLeaf.png) #F7E7CE top right / 80px no-repeat
+    background:  url(/LoadPageTopLeaf.png) #F7E7CE top right / 80px no-repeat;
+    color: #3C2708;
 }
 </style>
