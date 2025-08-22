7f9217f293f3b2bbe53b3fcc964910e6fa912076
Lovethrech <rachealdolapo45@gmail.com>
2025-08-22 06:45:15 +0100

    ...working on the project.

--- src/components/main/home/nav/OrderTab.vue
+++ src/components/main/home/nav/OrderTab.vue
@@ -3,7 +3,7 @@
     <div class="main-home-nav-order-tab-main-ctn">
         <RouterLink to="" class="link">
             <p class="main-home-nav-order-tab-main-ctn-desc">
-                Order
+                Orders
             </p>
         </RouterLink>    
     </div>
