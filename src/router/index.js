cebd596c72251a98468f105af1677969e6bab3c4
Lovethrech <rachealdolapo45@gmail.com>
2025-09-20 18:09:14 +0100

    ...working on all component.

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const router = createRouter({
     {
       path: '/orders',
       name: 'orders',
-      component: () => import('../views/Order')
+      component: () => import('../views/Orders')
     }
   ]
 })
