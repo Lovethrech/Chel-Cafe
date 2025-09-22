ef9041849c2cb47ab4e603cd406d5c91bd109eb8
Lovethrech <rachealdolapo45@gmail.com>
2025-09-22 18:13:40 +0100

    ...working on all component.

--- src/components/main/home/nav/OrderTab.vue
+++ src/components/main/home/nav/OrderTab.vue
@@ -1,7 +1,7 @@
 <template>
     
     <div class="main-home-nav-order-tab-main-ctn">
-        <RouterLink to="/order" class="link">
+        <RouterLink to="/orders" class="link">
             <p class="main-home-nav-order-tab-main-ctn-desc">
                 Orders
             </p>
