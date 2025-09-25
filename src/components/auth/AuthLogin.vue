0ea7f312a43d6e37a11af2c6110a9335f9daf42b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-25 18:23:01 +0100

    ...working on all component.

--- src/components/auth/AuthLogin.vue
+++ src/components/auth/AuthLogin.vue
@@ -28,7 +28,7 @@ const submitLoginDetails=()=>{
         </h2>
         <br/>
         <form 
-        action="" class="auth-login-main-ctn-form" @submit.prevent="submitLoginDetails">
+            action="" class="auth-login-main-ctn-form" @submit.prevent="submitLoginDetails">
             <div class="auth-login-main-ctn-form-input-and-label-ctn">
                 <label 
                     for="email" 
