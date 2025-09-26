0de9744ebe72c230df3003f498485cdb647634d8
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 19:14:43 +0100

    ...working on all component.

--- src/components/main/home/HomeOfferImg.vue
+++ src/components/main/home/HomeOfferImg.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="home-offer-img-ctn">
-        <img src="/public/green-cup-and-mushrooms.svg" alt="Green cup and Mushrooms" width="100" height="100"/>
+        <img 
+            src="/public/green-cup-and-mushrooms.svg" 
+            alt="Green cup and Mushrooms" 
+            width="100" 
+            height="100"
+        />
     </div>
 </template>
