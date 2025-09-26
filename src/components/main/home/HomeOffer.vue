cce106fce409ecd61c9a2ec211ddcfe1e0187169
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 19:20:32 +0100

    ...working on all component.

--- src/components/main/home/HomeOffer.vue
+++ src/components/main/home/HomeOffer.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+import HomeOfferImg from './HomeOfferImg.vue';
+import HomeOfferBtn from './HomeOfferBtn.vue';
 </script>
 
 <template>
     <div id="home-offer-main-ctn">
         <div id="home-offer-mini-ctn">
-
+            <HomeOfferImg></HomeOfferImg>
+            <HomeOfferBtn></HomeOfferBtn>
         </div>
     </div>
 </template>
