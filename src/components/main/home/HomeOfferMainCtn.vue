a4b6807fd8067339ce5d69e7ef49f13ab974d8cb
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 18:58:17 +0100

    ...working on all component.

--- src/components/main/home/HomeOfferMainCtn.vue
+++ src/components/main/home/HomeOfferMainCtn.vue
@@ -8,8 +8,14 @@
 
 <style scoped>
 #home-offer-main-ctn-main-ctn{
+    padding:2vh 4vw;
     width: 100%;
     height:30vh;
     background-color: #234523;
+}
+#home-offer-main-ctn-mini-ctn{
+    width: inherit;
+    height:inherit;
+    background-color: #122812;
 }
 </style>
