6341934d24b93c7b6b4355d22b4ad6b80736f7e6
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 18:28:59 +0100

    ...working on all component.

--- src/components/auth/AuthSignUp.vue
+++ src/components/auth/AuthSignUp.vue
@@ -31,7 +31,8 @@ import AuthSignUpType from './AuthSignUpType.vue';
         >
             <a 
                 href="" 
-                class="auth-signup-main-ctn-email-type-ctn-link">
+                class="auth-signup-main-ctn-email-type-ctn-link"
+            >
                 <p class="auth-signup-main-ctn-email-type-ctn-desc">
                     sign up with email
                 </p>
