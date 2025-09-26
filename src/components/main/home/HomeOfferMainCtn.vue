f6b8f14bb25453c1b9c6f277e21129b0e543385c
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 18:54:08 +0100

    ...working on all component.

--- src/components/main/home/HomeOfferMainCtn.vue
+++ src/components/main/home/HomeOfferMainCtn.vue
@@ -1,7 +1,11 @@
 <template>
     <section id="home-offer-main-ctn-main-ctn">
         <div id="home-offer-main-ctn-mini-ctn">
-            
+
         </div>
     </section>
 </template>
+
+<style scoped>
+
+</style>
