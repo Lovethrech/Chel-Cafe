a9756d93c402a1b379331a6de56d98100bec3e2f
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 18:27:49 +0100

    ...working on all component.

--- src/components/auth/AuthSignUp.vue
+++ src/components/auth/AuthSignUp.vue
@@ -8,7 +8,8 @@ import AuthSignUpType from './AuthSignUpType.vue';
         class="auth-signup-main-ctn"
     >
         <h3 
-            class="auth-signup-main-ctn-title">
+            class="auth-signup-main-ctn-title"
+        >
             or sign up using
         </h3>
         <br/>
