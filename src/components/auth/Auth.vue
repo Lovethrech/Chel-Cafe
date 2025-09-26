14dc330f54726aed635edf74bd5c937fef51d900
Lovethrech <rachealdolapo45@gmail.com>
2025-09-26 18:26:30 +0100

    ...working on all component.

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -4,7 +4,8 @@ import AuthSignUp from './AuthSignUp.vue';
 </script>
 
 <template>
-    <div class="auth-main-ctn">
+    <div 
+        class="auth-main-ctn">
         <div class="auth-mini-ctn">
             <AuthLogin></AuthLogin>
             <br/>
