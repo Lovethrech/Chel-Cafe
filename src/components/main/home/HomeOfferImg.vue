6f20bbc688be5c220f64484a43d4019a83169292
Lovethrech <rachealdolapo45@gmail.com>
2025-09-27 17:14:23 +0100

    ...working on all component.

--- src/components/main/home/HomeOfferImg.vue
+++ src/components/main/home/HomeOfferImg.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home-offer-img-ctn">
         <img 
-            src="/public/Why-Roast-Coffee-scaled 1.svg" 
+            src="/public/green-cup-and-mushrooms-last.svg" 
             alt="Green cup and Mushrooms" 
             width="100%"
             height="100%"
