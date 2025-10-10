b6a572ab7d52761446584a262be6fec4ad337432
Lovethrech <rachealdolapo45@gmail.com>
2025-10-10 12:28:54 +0100

    ...working on all component.

--- src/components/LoadPage.vue
+++ src/components/LoadPage.vue
@@ -6,7 +6,7 @@ import LoadPageText from './LoadPageText.vue';
     <div 
         class="load-page-main-ctn">
         <div 
-        class="load-page-mini-ctn">
+            class="load-page-mini-ctn">
             <LoadPageText></LoadPageText>
         </div>
     </div>
