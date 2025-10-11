06aef6388ac08d2e246e6bb8eada6ec4a40a85b3
Lovethrech <rachealdolapo45@gmail.com>
2025-10-11 12:31:15 +0100

    ...working on all component.

--- src/components/LoadPageText.vue
+++ src/components/LoadPageText.vue
@@ -3,7 +3,8 @@
         class="load-page-text-main-ctn"
     >
         <h1 
-            class="load-page-text-main-ctn-header">
+            class="load-page-text-main-ctn-header"
+            >
             Chel<br/>Cafe
         </h1>
     </div>
