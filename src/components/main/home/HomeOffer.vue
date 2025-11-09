6a48143f2145e9d752172c549354f051e5c9490d
Lovethrech <rachealdolapo45@gmail.com>
2025-11-09 11:32:46 +0100

    ...working on all components.

--- src/components/main/home/HomeOffer.vue
+++ src/components/main/home/HomeOffer.vue
@@ -34,7 +34,7 @@ const {name, imgSrc, link}=defineProps(['name', 'imgSrc', 'link']);
     justify-content: center;
     place-items: center;
     background-color: red;
-    width: 48%;
+    width: 25%;
     min-height: 30vh;
 }
 #home-offer-mini-ctn{
