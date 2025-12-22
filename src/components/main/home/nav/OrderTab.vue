daed60f5354303de452e5c2433646f5722422ec2
Lovethrech <rachealdolapo45@gmail.com>
2025-12-22 15:50:43 +0100

    ...working on all components.

--- src/components/main/home/nav/OrderTab.vue
+++ src/components/main/home/nav/OrderTab.vue
@@ -18,7 +18,7 @@
 <style scoped>
 .main-home-nav-order-tab-main-ctn{
     background-color: lavender;
-    margin: 0 4 5px;
+    margin: 0 4v 5px;
     width: 100%;
     display:flex;
     justify-content: right;
