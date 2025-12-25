58a0c72b48f4ea3b6ec2ebb9b094c3233da82ed5
Lovethrech <rachealdolapo45@gmail.com>
2025-12-25 15:56:14 +0100

    ...working on all components.

--- src/components/main/home/nav/OrderTab.vue
+++ src/components/main/home/nav/OrderTab.vue
@@ -28,7 +28,7 @@
 }
 .main-home-nav-order-tab-main-ctn-desc{
     margin-right: 3vw;
-    padding: 4v 10px;
+    padding: 4vh 10px;
     background-color: #3C2708;
     color:#fff;
     font-size: 12px;
